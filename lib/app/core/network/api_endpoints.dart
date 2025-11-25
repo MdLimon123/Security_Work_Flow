1973fc96f4af5d1a71d4e16d4d7486799c4d125a
sajid <sajidrec@gmail.com>
2025-11-25 15:29:00 +0600

    - add network client

---
+++ lib/app/core/network/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndpoints {
+  static const String _baseUrl = "http://10.10.12.15:8001/api";
+
+  static const String loginUrl = "$_baseUrl/auth/login/";
+  static const String signupUrl = "$_baseUrl/auth/login/";
+}
